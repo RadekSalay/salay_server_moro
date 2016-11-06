2f5d47eb36a8f16781f68e68921679cdfb0eb366
RadekSalay <radek.salay@gmail.com>
2016-11-06 19:16:21 +0100

    server init

---
+++ src/main/java/com/salay/SalayServerMoroApplication.java
@@ -0,0 +1,12 @@
+package com.salay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalayServerMoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalayServerMoroApplication.class, args);
+	}
+}
